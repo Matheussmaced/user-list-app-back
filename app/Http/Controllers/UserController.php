7b0029bcfc921d808c9b3225eb1732ff583e00e6
Matheus <mateusbyakuya3@gmail.com>
2026-02-02 20:35:51 -0300

    feat: creating validação para o metodo post

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -20,9 +21,9 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
-        //
+        dd($request->validated());
     }
 
     /**
